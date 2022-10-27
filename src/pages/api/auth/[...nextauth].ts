56f99ae140bd2e22b93ce611fca088952864f572
Christian <pleasefloyd73@gmail.com>
2022-10-27 17:51:50 -0300

    fix: removed auth config in nextauth.ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,15 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
-        url: 'https://github.com/login/oauth/authorize',
         params: { scope: 'read:user' }
       }
     })
-    // ...add more providers here
   ]
 }
+
 export default NextAuth(authOptions)
